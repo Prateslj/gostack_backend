1bcc5e803a64b83179b59388a475b8f3805697f1
lprates_2020 <lprates@gmail.com>
2020-08-17 16:30:43 -0300

    HTTP requests, Params types and destructuring

--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello world!' });
+});
+
+app.get('/projects', (request, response) => {
+    const { title, owner } = request.query;
+
+    console.log(title);
+    console.log(owner);
+
+    return response.json(['project1','project2']);
+});
+
+app.post('/projects', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    return response.json({ massage: "Project created successfuly!"});
+});
+
+app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+
+    console.log(params);
+
+    return response.json({ massage: "Project modified."});
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json({ massage: "Project deleted."});
 });
 
 app.listen(3333, () => {
